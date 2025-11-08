9923030dc40a421cfc7c55f11805f6a142fd2a6f
LuMariGames <kai.hono426@gmail.com>
2025-11-08 14:08:23 +0900

    Update types.h

--- libctru/include/3ds/types.h
+++ libctru/include/3ds/types.h
@@ -18,15 +18,15 @@
 #endif
 #endif
 
-typedef uint8_t u8;   ///<  8-bit unsigned integer
-typedef uint16_t u16; ///< 16-bit unsigned integer
-typedef uint32_t u32; ///< 32-bit unsigned integer
-typedef uint64_t u64; ///< 64-bit unsigned integer
+typedef uint_fast8_t u8;   ///<  8-bit unsigned integer
+typedef uint_fast16_t u16; ///< 16-bit unsigned integer
+typedef uint_fast32_t u32; ///< 32-bit unsigned integer
+typedef uint_fast64_t u64; ///< 64-bit unsigned integer
 
-typedef int8_t s8;   ///<  8-bit signed integer
-typedef int16_t s16; ///< 16-bit signed integer
-typedef int32_t s32; ///< 32-bit signed integer
-typedef int64_t s64; ///< 64-bit signed integer
+typedef int_fast8_t s8;   ///<  8-bit signed integer
+typedef int_fast16_t s16; ///< 16-bit signed integer
+typedef int_fast32_t s32; ///< 32-bit signed integer
+typedef int_fast64_t s64; ///< 64-bit signed integer
 
 typedef volatile u8 vu8;   ///<  8-bit volatile unsigned integer.
 typedef volatile u16 vu16; ///< 16-bit volatile unsigned integer.
